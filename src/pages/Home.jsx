74d13de199b49f02928624677c87950559fd1e32
NMartinez0399 <97861596+NMartinez0399@users.noreply.github.com>
2025-05-24 13:45:27 -0700

    created ThemeToggle.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
     return (
-        <div className="min-h-screen container">
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
             {/* Theme Toggle */}
+            <ThemeToggle />
 
             {/* Background effect */}
 
